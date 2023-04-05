4b2c932c78f35bb5bb4fc288baaf1960906574ac
CvetiTo <email@example.com>
2023-04-05 17:18:21 +0300

    First commit

---
+++ components/Layout.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Head from 'next/head.js';
+import NavBar from './NavBar.jsx';
+import Footer from './Footer.jsx';
+
+const Layout = ({ children }) => {
+  return (
+    <div className='layout'>
+      <Head>
+        <title>JS Best Store</title>
+      </Head>
+      <header>
+        <NavBar />
+      </header>
+      <main className='main-container'>
+        { children }
+      </main>
+      <footer>
+        < Footer />
+      </footer>
+    </div>
+  )
+}
+
+export default Layout
